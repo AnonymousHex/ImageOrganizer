8a411405f69f885ab37501fa976460e12b090b19
AnonymousHex <33821968+AnonymousHex@users.noreply.github.com>
2018-01-15 23:58:57 -0800

    Added groundwork for uniform scrollbar.

---
+++ ImageOrganizer/Controls/UniformScrollBar.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace ImageOrganizer.Controls
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class UniformScrollBar : ScrollBar
+	{
+		static UniformScrollBar()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(UniformScrollBar), new FrameworkPropertyMetadata(typeof(UniformScrollBar)));
+		}
+	}
+}
